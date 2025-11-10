b4a083216ef35be04346278e0a747cf5bfcb0407
Victor Fernandes <70545624+devhmalu@users.noreply.github.com>
2025-11-10 12:52:12 +0000

    Motivação

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
-import Foto from "/workspaces/clone-tabnews/pages/img/motivacao.jpg";
+import Foto from "./img/motivacao.jpg";
 
 function Home() {
     return (
     <>
       <h1>O motivo da minha dedicação, é você!</h1>
-      <img src={Foto} alt="Motivo da dedicação" />
+      <img src= "https://raw.githubusercontent.com/devhmalu/clone-tabnews/main/pages/img/motivacao.jpg" alt="Motivo da dedicação" width={600} />
     </>
   );
 }
