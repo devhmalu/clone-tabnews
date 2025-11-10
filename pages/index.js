5257f7e74e4f1f6092983fe366c811547a94d7a8
Victor Fernandes <70545624+devhmalu@users.noreply.github.com>
2025-11-10 11:04:14 +0000

    add

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,10 @@ import Foto from "./img/motivacao.jpg"
 
 function Home() {
     return (
-    <div>
+    <>
       <h1>O motivo da minha dedicação, é você!</h1>
-      <img
-        src={Foto}
-        alt="Motivo da dedicação"
-        width="300"
-      />
-    </div>
+      <img src={Foto} alt="Motivo da dedicação" />
+    </>
   );
 }
 
